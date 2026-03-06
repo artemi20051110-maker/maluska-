40063305a5b8b54568abbad9448f7cc397cf2abc
artemi20051110-maker <artemi20051110@gmail.com>
2026-03-06 17:53:42 +0300

    update style and logic

---
+++ deploy.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "autodeploy"
+git push origin main
---
+++ push.bat
@@ -0,0 +1,6 @@
+@echo off
+git add .
+git commit -m "update style and logic"
+git push origin main
+echo Гитхаб обновлен! Жди 30 секунд.
+pause
